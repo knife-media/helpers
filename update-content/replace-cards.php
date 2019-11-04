5be1a5ef28755715386e7f3639bbe2a712feea40
Anton Lukin <anton@lukin.me>
2019-11-04 12:22:46 +0300

    add replace-cards helper

---
+++ update-content/replace-cards.php
@@ -0,0 +1,38 @@
+<?php
+
+if(php_sapi_name() !== 'cli') {
+    exit;
+}
+
+$_SERVER = [
+    "SERVER_PROTOCOL" => "HTTP/1.1",
+    "HTTP_HOST"       => "knife.media",
+    "SERVER_NAME"     => "knife.media",
+    "REQUEST_URI"     => "/",
+    "REQUEST_METHOD"  => "GET"
+];
+
+define('WP_CACHE', false);
+define('WP_DEBUG', true);
+define('WP_USE_THEMES', false);
+
+require( __DIR__ . '/../../wordpress/wp-load.php');
+
+
+{
+    global $wbdb;
+
+    $posts = $wpdb->get_results("SELECT id, post_content FROM {$wpdb->posts} WHERE post_status = 'publish' AND post_content LIKE '%[card]%' ORDER BY id ASC");
+
+    foreach($posts as $post) {
+        $content = $post->post_content;
+
+        $content = preg_replace('~\s*\[/card\]\s*?\[card\]\s*~is', "\n<!--card-->\n", $content);
+        $content = str_replace(['[/card]', '[card]'], '', $content);
+
+        $content = esc_sql($content);
+
+        $wpdb->query("UPDATE {$wpdb->posts} SET post_content = '{$content}' WHERE ID = " . absint($post->id));
+        echo 'https://knife.plus/?p=' . $post->id . "\n";
+    }
+}
